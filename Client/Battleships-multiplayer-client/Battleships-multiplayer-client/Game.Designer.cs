c513f10f0ff479239cc8c0cb89492ac7be58d2ad
Jack <71041042+jackakay@users.noreply.github.com>
2024-07-26 20:39:05 +0100

    Basic edits
    Made gui nicer implemented a grid

--- Client/Battleships-multiplayer-client/Battleships-multiplayer-client/Game.Designer.cs
+++ Client/Battleships-multiplayer-client/Battleships-multiplayer-client/Game.Designer.cs
@@ -30,6 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
             pictureBox1 = new PictureBox();
+            richTextBox1 = new RichTextBox();
+            checkBox1 = new CheckBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -37,27 +39,58 @@
             // 
             pictureBox1.BackgroundImageLayout = ImageLayout.None;
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(-110, -280);
+            pictureBox1.Location = new Point(-2, -3);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(1610, 1405);
+            pictureBox1.Size = new Size(873, 927);
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
+            pictureBox1.Paint += pictureBox1_Paint;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(651, 97);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(139, 533);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.BackColor = Color.Transparent;
+            checkBox1.Location = new Point(62, 65);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(126, 19);
+            checkBox1.TabIndex = 3;
+            checkBox1.Text = "Show enemies grid";
+            checkBox1.UseVisualStyleBackColor = false;
             // 
             // Game
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(847, 717);
+            Controls.Add(checkBox1);
+            Controls.Add(richTextBox1);
             Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
             Name = "Game";
             Text = "Game";
+            Load += Game_Load;
+            Resize += Game_Resize;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox1;
+        private RichTextBox richTextBox1;
+        private CheckBox checkBox1;
     }
 }
--- Client/Battleships-multiplayer-client/Battleships-multiplayer-client/Game.cs
+++ Client/Battleships-multiplayer-client/Battleships-multiplayer-client/Game.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,14 +13,53 @@ namespace Battleships_multiplayer_client
 {
     public partial class Game : Form
     {
+        const string alphabet = "abcdefghijklmnopqrstuvwxyz";
         public Game()
         {
             InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.DoubleBuffered = true;
+
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+            this.Invalidate();
+            pictureBox1.Size = Screen.FromControl(this).WorkingArea.Size;
+            pictureBox1.Location = new Point { X = 0, Y = 0 };
+            pictureBox1.SendToBack();
+
+        }
+
+
+
+
+
+        private void Game_Resize(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            const int GRID_LENGTH = 50;
+            Pen pen = new Pen(Color.Black, 2);
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    e.Graphics.DrawRectangle(pen, (x * GRID_LENGTH) + 100, (y * GRID_LENGTH) + 100, GRID_LENGTH, GRID_LENGTH);
+                }
+            }
         }
     }
 }
