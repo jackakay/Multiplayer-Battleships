44653f90a148ae491cf7fd0fd037f8a2bba8602b
Jack <71041042+jackakay@users.noreply.github.com>
2024-08-01 16:44:31 +0100

    project

--- Client/Battleships-multiplayer-client/Battleships-multiplayer-client/Game.Designer.cs
+++ Client/Battleships-multiplayer-client/Battleships-multiplayer-client/Game.Designer.cs
@@ -32,6 +32,12 @@
             pictureBox1 = new PictureBox();
             richTextBox1 = new RichTextBox();
             checkBox1 = new CheckBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            listBox1 = new ListBox();
+            richTextBox2 = new RichTextBox();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -39,19 +45,20 @@
             // 
             pictureBox1.BackgroundImageLayout = ImageLayout.None;
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(-2, -3);
+            pictureBox1.Location = new Point(-11, -4);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(873, 927);
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
             pictureBox1.Paint += pictureBox1_Paint;
+            
             // 
             // richTextBox1
             // 
             richTextBox1.Location = new Point(651, 97);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(139, 533);
+            richTextBox1.Size = new Size(184, 280);
             richTextBox1.TabIndex = 2;
             richTextBox1.Text = "";
             richTextBox1.TextChanged += richTextBox1_TextChanged;
@@ -59,7 +66,8 @@
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.BackColor = Color.Transparent;
+            checkBox1.BackColor = SystemColors.ActiveCaption;
+            checkBox1.ForeColor = SystemColors.Control;
             checkBox1.Location = new Point(62, 65);
             checkBox1.Name = "checkBox1";
             checkBox1.Size = new Size(126, 19);
@@ -67,11 +75,86 @@
             checkBox1.Text = "Show enemies grid";
             checkBox1.UseVisualStyleBackColor = false;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaption;
+            label1.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.Control;
+            label1.Location = new Point(273, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(299, 50);
+            label1.TabIndex = 4;
+            label1.Text = "Placement phase";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ActiveCaption;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.Control;
+            label2.Location = new Point(651, 54);
+            label2.Name = "label2";
+            label2.Size = new Size(74, 30);
+            label2.TabIndex = 6;
+            label2.Text = "Moves";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.ActiveCaption;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = SystemColors.Control;
+            label3.Location = new Point(651, 445);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 30);
+            label3.TabIndex = 7;
+            label3.Text = "Fleet";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Items.AddRange(new object[] { "Minesweep (2 squares) ", "Frigate (3 squares) ", "Cruiser  (4 squares) ", "Battleship (5 squares)  " });
+            listBox1.Location = new Point(651, 498);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(159, 109);
+            listBox1.TabIndex = 8;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(651, 613);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(159, 96);
+            richTextBox2.TabIndex = 9;
+            richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.FlatAppearance.BorderColor = SystemColors.ActiveCaption;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.Control;
+            button1.Location = new Point(358, 674);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 35);
+            button1.TabIndex = 10;
+            button1.Text = "Ready";
+            button1.UseVisualStyleBackColor = false;
+            // 
             // Game
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(847, 717);
+            Controls.Add(button1);
+            Controls.Add(richTextBox2);
+            Controls.Add(listBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(checkBox1);
             Controls.Add(richTextBox1);
             Controls.Add(pictureBox1);
@@ -81,7 +164,8 @@
             Name = "Game";
             Text = "Game";
             Load += Game_Load;
-            Resize += Game_Resize;
+            
+            
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -92,5 +176,11 @@
         private PictureBox pictureBox1;
         private RichTextBox richTextBox1;
         private CheckBox checkBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private ListBox listBox1;
+        private RichTextBox richTextBox2;
+        private Button button1;
     }
 }
--- Client/Battleships-multiplayer-client/Battleships-multiplayer-client/Game.cs
+++ Client/Battleships-multiplayer-client/Battleships-multiplayer-client/Game.cs
@@ -8,18 +8,128 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing.Drawing2D;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.LinearAlgebra;
+
+
 
 namespace Battleships_multiplayer_client
 {
     public partial class Game : Form
     {
+        [DllImport("user32.dll")]
+        static extern bool GetAsyncKeyState(int vKey);
+
         const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        string[] ships = { "Minesweep", "Frigate", "Cruiser", "Battleship" };
+        public PictureBox currentActiveShip = null;
+        public List<PictureBox> fleet = new List<PictureBox>();
+        public List<PictureBox> placedFleet = new List<PictureBox>();
+        public List<int> shipsAvailable = new List<int> { 4, 3, 2, 1 };
+        const int GRID_LENGTH = 50;
+        System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        int count = 0;
+
         public Game()
         {
             InitializeComponent();
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             this.DoubleBuffered = true;
 
+            myTimer.Interval = 100;
+            myTimer.Tick += timeTick;
+
+        }
+
+        private void timeTick(object? sender, EventArgs e)
+        {
+            if (currentActiveShip != null)
+            {
+
+                if (!placedFleet.Contains(currentActiveShip))
+                {
+                    if (GetAsyncKeyState(87))
+                    {
+                        //up
+                        if (currentActiveShip.Location.Y - GRID_LENGTH >= 100) currentActiveShip.Location = new Point { X = currentActiveShip.Location.X, Y = currentActiveShip.Location.Y - GRID_LENGTH };
+                    }
+                    if (GetAsyncKeyState(83))
+                    {
+                        //down
+                        if (currentActiveShip.Location.Y + (currentActiveShip.Height) < 100 + (10 * GRID_LENGTH)) currentActiveShip.Location = new Point { X = currentActiveShip.Location.X, Y = currentActiveShip.Location.Y + GRID_LENGTH };
+                    }
+                    if (GetAsyncKeyState(68))
+                    {
+                        //right
+                        if (currentActiveShip.Location.X + GRID_LENGTH <= (100 - currentActiveShip.Width) + (10 * GRID_LENGTH)) currentActiveShip.Location = new Point { X = currentActiveShip.Location.X + GRID_LENGTH, Y = currentActiveShip.Location.Y };
+                    }
+                    if (GetAsyncKeyState(65))
+                    {
+                        //left
+                        if (currentActiveShip.Location.X - GRID_LENGTH >= 100) currentActiveShip.Location = new Point { X = currentActiveShip.Location.X - GRID_LENGTH, Y = currentActiveShip.Location.Y };
+                    }
+                    if (GetAsyncKeyState(82))
+                    {
+                        if (count == 0)
+                        {
+                            if (currentActiveShip.Location.Y <= GRID_LENGTH * (10 + 2 - currentActiveShip.Width / GRID_LENGTH))
+                            {
+                                count++;
+                                currentActiveShip.Width = GRID_LENGTH;
+                                currentActiveShip.Height = (listBox1.SelectedIndex + 2) * GRID_LENGTH;
+                            }
+                        }
+                        else if (count == 1)
+                        {
+                            if (currentActiveShip.Location.X <= GRID_LENGTH * (10 + 2 - currentActiveShip.Height / GRID_LENGTH))
+                            {
+                                count--;
+                                currentActiveShip.Width = (listBox1.SelectedIndex + 2) * GRID_LENGTH;
+                                currentActiveShip.Height = GRID_LENGTH;
+                            }
+                        }
+
+
+                    }
+                    if (GetAsyncKeyState(13))
+                    {
+                        bool intersects = false;
+                        foreach(PictureBox ship in placedFleet)
+                        {
+                            if (currentActiveShip.Bounds.IntersectsWith(ship.Bounds))
+                            {
+                                intersects = true;
+                            }
+                        }
+                        if (!intersects)
+                        {
+                            placedFleet.Add(currentActiveShip);
+                            bool x = false;
+                            foreach (int i in shipsAvailable)
+                            {
+                                if (i == 0)
+                                {
+                                    x = true;
+                                }
+                                else
+                                {
+                                    x = false;
+                                }
+                            }
+                            if (x)
+                            {
+                                button1.Visible = true;
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Cannot place a ship over the bounds off another ship");
+                        }
+                    }
+                }
+            }
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -38,20 +148,20 @@ namespace Battleships_multiplayer_client
             pictureBox1.Size = Screen.FromControl(this).WorkingArea.Size;
             pictureBox1.Location = new Point { X = 0, Y = 0 };
             pictureBox1.SendToBack();
-
+            myTimer.Start();
+            populateListBox();
+            button1.Visible = false;
         }
 
 
 
 
 
-        private void Game_Resize(object sender, EventArgs e)
-        {
-        }
+        
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            const int GRID_LENGTH = 50;
+
             Pen pen = new Pen(Color.Black, 2);
             for (int x = 0; x < 10; x++)
             {
@@ -61,5 +171,67 @@ namespace Battleships_multiplayer_client
                 }
             }
         }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (shipsAvailable[listBox1.SelectedIndex] > 0)
+            {
+                PictureBox ship = new PictureBox { Location = new Point { X = 100, Y = 100 }, Size = new Size { Width = GRID_LENGTH * (listBox1.SelectedIndex + 2), Height = GRID_LENGTH }, BackColor = Color.Black };
+                this.Controls.Add(ship);
+                ship.BringToFront();
+                fleet.Add(ship);
+                currentActiveShip = ship;
+
+                shipsAvailable[listBox1.SelectedIndex]--;
+                populateListBox();
+            }
+        }
+        private void populateListBox()
+        {
+            richTextBox2.Clear();
+            string[] items = new string[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                items[i] = (ships[i] + " (" + (i + 2).ToString() + " squares) x" + shipsAvailable[i]).ToString();
+                richTextBox2.AppendText(items[i] + "\n");
+            }
+
+
+        }
+        private string exportGameDataJson()
+        {
+            List<Ship> shipList = new List<Ship>();
+            foreach(PictureBox ship in placedFleet)
+            {
+                Point start = ship.Location;
+                Point end = ship.Location;
+                if (ship.Height == GRID_LENGTH)
+                {
+                    end = new Point { X = ship.Location.X + ship.Width, Y = ship.Location.Y };
+
+                }
+                else if (ship.Width == GRID_LENGTH)
+                {
+                    end = new Point { X = ship.Location.X, Y = ship.Location.Y+ship.Location.Y };
+                }
+                Ship s = new Ship { start = start, end = end };
+            }
+        }
+       
+        
+
+        
+    }
+    public class Map
+    {
+        public List<Ship> fleet { get; set; }
+    }
+
+    public class Ship
+    {
+        public Point start { get; set; }
+        public Point end { get; set; }
     }
 }
